d4618da97a262ce6a45251713778165f8833857c
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2016-03-15 09:23:24 +0100

    use node crypto module

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+#!/usr/bin/env node
 // Nodejs encryption of buffers
-var yargs = require('yargs')
-var crypto = require('crypto');
+const yargs = require('yargs')
+const ProgressBar = require('progress');
+const inquirer = require("inquirer");
+const crypto = require('crypto');
     
 var DEFAULT_ALGORITHM = 'aes-256-ctr';
 var EPILOGUE_TEXT = "Developed by Pachito Marco Calabrese"
@@ -12,11 +15,37 @@ var zlib = require('zlib');
 //r.pipe(zip).pipe(encrypt).pipe(decrypt).pipe(unzip).pipe(w);
 //r.pipe(encrypt).pipe(w);
 
+function bar(stream,file,encrypt) {
+    var fileSize = fs.statSync(file).size;
+    var barOpts = {
+        width: 20,
+        total: fileSize,
+        clear: false
+    };
+    var bar = new ProgressBar((encrypt ? 'encrypting' : 'decrypting') + ' [:bar] :percent :etas', barOpts);
+    stream.on('data', function (chunk) {
+        bar.tick(chunk.length);
+    });
+}
+
+var questions = [{
+    type: 'password',
+    name: 'password',
+    message: 'Type your encrypt/decrypt password'
+},{
+    type: 'list',
+    name: 'algorithm',
+    message: 'Choose an encrypt/decrypt algorithm',
+    default: DEFAULT_ALGORITHM,
+    choices: crypto.getCiphers()
+}]
+
 /* commands */
 yargs
 .usage('Usage: $0 <command> [options]')
 .command('encrypt','Encrypt file', function(yargs) {
     return yargs
+    .demand(2)
     .option('password', {
         alias: 'p',
         describe: 'Encrypt Password'
@@ -27,17 +56,22 @@ yargs
         default: DEFAULT_ALGORITHM,
         demand: false
     })
-    .usage("encrypt --password=[PASSWORD] <INPUT_FILE> <OUTPUT_FILE>")
+    .usage("$0 encrypt <INPUT_FILE> [<OUTPUT_FILE>]")
     .help()
 },function (argv) {
-    var r = fs.createReadStream(argv._[1] || 'file.txt');
-    var encrypt = crypto.createCipher(argv.algorithm, argv.password);
-    var w = fs.createWriteStream(argv._[2] || 'file.out.txt');
-    r.pipe(encrypt).pipe(w);
-    console.log(argv._[1]+" encrypted, "+argv._[2]+" generated");
+    inquirer.prompt(questions, function( answers ) {
+        // Use user feedback for... whatever!! 
+        const cipher = crypto.createCipher(answers.algorithm, answers.password);
+        var r = fs.createReadStream(argv._[1] || 'file.txt');
+        var w = fs.createWriteStream(argv._[2] || argv._[1]+'.encrypted');
+        bar(r,argv._[1] || 'file.txt', true)
+        r.pipe(cipher).pipe(w);
+        console.log(argv._[1]+" encrypted, "+(argv._[2] || argv._[1]+'.encrypted'));
+    });
 })
 .command('decrypt','Descrypt file', function(yargs) {
     return yargs
+    .demand(2)
     .option('password', {
         alias: 'p',
         describe: 'Encrypt Password'
@@ -48,20 +82,27 @@ yargs
         default: DEFAULT_ALGORITHM,
         demand: false
     })
-    .usage("decrypt --password=[PASSWORD] <INPUT_FILE> <OUTPUT_FILE>")
+    .usage("decrypt <INPUT_FILE> [<OUTPUT_FILE>]")
     .help()
 },function (argv) {
-    var r = fs.createReadStream(argv._[1] || 'file.txt');
-    var decrypt = crypto.createDecipher(argv.algorithm, argv.password)
-    var w = fs.createWriteStream(argv._[2] || 'file.out.txt');
-    r.pipe(decrypt).pipe(w);
-    console.log(argv._[1]+" decrypted, "+argv._[2]+" generated");
+    inquirer.prompt(questions, function( answers ) {
+        const decipher = crypto.createDecipher(answers.algorithm, answers.password);
+        var r = fs.createReadStream(argv._[1] || 'file.txt');
+        var w = fs.createWriteStream(argv._[2] || argv._[1]+'.decrypted');
+        bar(r,argv._[1] || 'file.txt', false)
+        r.pipe(decipher).pipe(w);
+        console.log(argv._[1]+" decrypted, "+(argv._[2] || argv._[1]+'.decrypted'));
+    });
 })
-.demand(1)
-.example("$0 encrypt --password=[PASSWORD] <INPUT_FILE> <OUTPUT_FILE>")
-.demand('p')
-.alias('p', 'password')
-.nargs('p', 1)
-.describe('p', 'Encrypt/Decrypt Password')
+.demand(2)
+.example("$0 <COMMAND> [--password] [--algorithm] <INPUT_FILE> <OUTPUT_FILE>")
+.demand('password')
+.alias('password', 'p')
+.nargs('password', 1)
+.describe('password', 'Encrypt/Decrypt Password')
+.alias('algorithm', 'a')
+.nargs('algorithm', 1)
+.describe('algorithm', 'Encryption/Decryption Algorithm')
+.choices('algorithm', [DEFAULT_ALGORITHM,])
 .epilogue(EPILOGUE_TEXT)
 .argv;
